75de4d9f0f778b33a7308991335a24639fa6d37c
StarrMex <01.cjneo@gmail.com>
2025-05-26 16:03:49 +0800

    Assignement files

---
+++ assignment4/question1.js
@@ -0,0 +1,12 @@
+const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
+
+function findMax() {
+    let highestPrice = amdPrices[0];
+
+    for (let price of amdPrices) {
+        if (price > highestPrice) {
+            highestPrice = price;
+        }
+    }
+    console.log("AMD 52-week high is " + highestPrice);
+}
