cb05de0a6a311e07363f8bd1205cd29508bfea77
StarrMex <99953918+StarrMex@users.noreply.github.com>
2025-05-26 17:19:01 +0800

    Restore question1.js

--- assignment4/question1.js
+++ assignment4/question1.js
@@ -1,3 +1,12 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
 // Write your code below
+function highestPrice() {
+  let maxPrice = amdPrices[0];
+  for (let price > maxPrice) {
+    maxPrice = price;
+    }
+  }
+  console.log("AMD 52-wk high is " + maxPrice);
+}
+highestPrice();
